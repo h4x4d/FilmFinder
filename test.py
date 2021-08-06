32efff716574cb765bed1a84957f026fe6851c03
h4x4d <cleanyboom@gmail.com>
2021-08-06 21:13:25 +0300

    fixes

--- test.py
+++
@@ -1,7 +0,0 @@
-import datetime
-import sqlite3
-
-conn = sqlite3.connect('data.db')
-cur = conn.cursor()
-cur.execute(f'SELECT * FROM films WHERE id in ({", ".join(["1", "2"])})')
-print(cur.fetchall())
